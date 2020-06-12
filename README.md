043210ad0a848a3182bce4bd20eabb5e4db43745
wangkai <184641801@qq.com>
2020-06-12 17:39:07 +0800

    complete

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-"# vue3-demo" 
+# vue3-demo
+
+使用 vue3 的新 API 开发一款 demo,以便熟悉 vue3 相关特性的使用
+
+![image](https://github.com/kaygod/blog_demo/blob/master/Video_00017.gif)
+
+## 1.下载项目,运行 npm intall 安装依赖
+
+## 2.运行 npm run serve
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,62 +1,71 @@
 <template>
   <div class="home">
-    <Header/>
-    <Product :items="likes" @inquire="inquire"/>
-    <Product :items="recommends" @inquire="inquire"/>
+    <Header :has_back="false" />
+    <Product :items="likes" @inquire="inquire" />
+    <Product :items="recommends" @inquire="inquire" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Header from '@/components/Header.vue'
-import axios from 'axios';
-import { ref,onMounted } from "vue";
+import Header from "@/components/Header.vue";
+import { ref, onMounted } from "vue";
 import Product from "../components/Product";
+import { useRouter } from "vue-router";
+import { post } from "../util/tool";
 
 export default {
-  name: 'Home',
   components: {
     Header,
     Product
   },
-  setup(){
-      const { likes,recommends } = useData();
-
-      function inquire(data){
-        const {name,price} = data;
-        alert(`今天${name}的价格是${price}元`);
-      }
-
-      return {
-        likes,
-        recommends,
-        inquire
-      }
+  setup() {
+    const { likes, recommends } = useData();
+    const { inquire } = useDetailPage();
+    return {
+      likes,
+      recommends,
+      inquire
+    };
   }
+};
+
+/**
+ * 进入详情页面相关操作
+ */
+function useDetailPage() {
+  const router = useRouter();
+  function inquire(data) {
+    const { id } = data;
+    router.push(`/detail/${id}`);
+  }
+  return {
+    inquire
+  };
 }
 
-function useData(){
- let likes = ref({});
- let recommends = ref({});
+function useData() {
+  let likes = ref({});
+  let recommends = ref({});
 
- onMounted(() => {
-   init();
- })
+  onMounted(() => {
+    init();
+  });
 
- function init(){
-    return new Promise((resolve)=>{
-      axios.get('/api/home').then((data)=>{
-       const result = data.data; 
-       likes.value =  result.likes;
-       recommends.value = result.recommends;
-      })
-    })
+  function init() {
+    return new Promise(resolve => {
+      post({
+        url: "/api/home"
+      }).then(result => {
+        likes.value = result.likes;
+        recommends.value = result.recommends;
+      });
+    });
   }
 
   return {
     likes,
     recommends
-  }  
-  
+  };
 }
 </script>
